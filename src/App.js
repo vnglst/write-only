b88d0e4e5ad729664442c1782c857a8c7c6f4ebc
Koen van Gilst <koen.vangilst@gmail.com>
2019-02-16 18:58:51 +0100

    minor styling tweaks

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useUndoableReducer, UNDO, REDO } from './useUndoableReducer'
 const UPDATE = 'update'
 const CLEAR = 'clear'
 const RESET = 'reset'
-const CACHE_KEY = 'write-only-storage'
+
+const CACHE_KEY = 'app-storage'
 const INITIAL_TEXT =
   'Then there was the bad weather. It would come in one day when the fall was over. We would have to shut the windows in the night against the rain and the cold wind would strip the leaves from the trees in the Place Contrescarpe. The leaves lay sodden in the rain and the wind drove the rain against the big green autobus at the terminal and the Café des Amateurs was crowded and the windows misted over from the heat and the smoke inside.'
 
@@ -17,7 +18,7 @@ function reducer(state, action) {
     case RESET:
       return { text: INITIAL_TEXT }
     default:
-      throw new Error('Unknown action')
+      throw new Error(`Unknown action ${action.type}`)
   }
 }
 
@@ -34,6 +35,7 @@ function App() {
     reducer,
     getInitialState()
   )
+
   useEffect(
     () => window.localStorage.setItem(CACHE_KEY, JSON.stringify(state)),
     [state] // only save in locatstorage if state changes
