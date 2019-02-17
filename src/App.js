a7e35f6c2547ef5f53e9317eab71de9dbdc5d85c
Koen van Gilst <koen.vangilst@gmail.com>
2019-02-17 14:21:08 +0100

    adds unit tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const CLEAR = 'clear'
 const RESET = 'reset'
 const ABOUT = 'about'
 
-const CACHE_KEY = 'app-storage'
+const CACHE_KEY = 'write-only-app-storage'
 
 function reducer(state, action) {
   switch (action.type) {
@@ -25,8 +25,13 @@ function reducer(state, action) {
 }
 
 function getInitialState() {
-  const storedState = JSON.parse(window.localStorage.getItem(CACHE_KEY))
-  if (storedState) return storedState
+  let storedState
+  try {
+    storedState = JSON.parse(window.localStorage.getItem(CACHE_KEY))
+  } catch (error) {
+    console.error('Error restoring from localStorage, using default state')
+  }
+  if (storedState && storedState.text) return storedState
   return {
     text: INITIAL_TEXT
   }
@@ -48,21 +53,44 @@ function App() {
   return (
     <>
       <nav className="Controls">
-        <button type="button" onClick={e => dispatch({ type: CLEAR })}>
+        <button
+          data-testid="clear-button"
+          type="button"
+          onClick={e => dispatch({ type: CLEAR })}
+        >
           Clear
         </button>
-        <button onClick={e => dispatch({ type: RESET })}>Reset</button>
-        <button disabled={!canUndo} onClick={e => dispatch({ type: UNDO })}>
+        <button
+          data-testid="reset-button"
+          onClick={e => dispatch({ type: RESET })}
+        >
+          Reset
+        </button>
+        <button
+          data-testid="undo-button"
+          disabled={!canUndo}
+          onClick={e => dispatch({ type: UNDO })}
+        >
           Undo
         </button>
-        <button disabled={!canRedo} onClick={e => dispatch({ type: REDO })}>
+        <button
+          data-testid="redo-button"
+          disabled={!canRedo}
+          onClick={e => dispatch({ type: REDO })}
+        >
           Redo
         </button>
-        <button onClick={e => dispatch({ type: ABOUT })}>About</button>
+        <button
+          data-testid="about-button"
+          onClick={e => dispatch({ type: ABOUT })}
+        >
+          About
+        </button>
       </nav>
       <textarea
         autoFocus
         value={text}
+        data-testid="textarea"
         onChange={e => dispatch({ type: UPDATE, text: e.target.value })}
       />
     </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { render, fireEvent, cleanup } from 'react-testing-library'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import App from './App'
+
+let textarea
+let getByTestId
+let utils
+
+beforeEach(() => {
+  utils = render(<App />)
+  getByTestId = utils.getByTestId
+  textarea = getByTestId('textarea')
+})
+
+afterEach(() => {
+  // TODO: add tests for localStorage and it's edge cases
+  window.localStorage.clear()
+  cleanup()
+})
+
+it('should render App with default text', () => {
+  expect(textarea.textContent).toContain('Then there was the bad weather.')
+})
+
+it('should clear all text when Clear is pressed', () => {
+  const clear = getByTestId('clear-button')
+  fireEvent.click(clear)
+  expect(textarea.textContent).toBe('')
+})
+
+it('should show about text when About is pressed', () => {
+  const about = getByTestId('about-button')
+  fireEvent.click(about)
+  expect(textarea.textContent).toContain('Write Only is')
+})
+
+it('should reset to initial text when Reset is pressed', () => {
+  const clear = getByTestId('clear-button')
+  fireEvent.click(clear)
+  const reset = getByTestId('reset-button')
+  fireEvent.click(reset)
+  expect(textarea.textContent).toContain('Then there was the bad weather.')
+})
+
+it('should be possible to add text', () => {
+  fireEvent.change(textarea, { target: { value: 'Hello world' } })
+  expect(textarea.textContent).toBe('Hello world')
+})
+
+it('should be possible to undo any text changes', () => {
+  fireEvent.change(textarea, { target: { value: 'Hello world' } })
+  const undo = getByTestId('undo-button')
+  fireEvent.click(undo)
+  expect(textarea.textContent).toContain('Then there was the bad weather.')
+})
+
+it('should not crash or change when pressing undo twice', () => {
+  fireEvent.change(textarea, { target: { value: 'Hello world' } })
+  const undo = getByTestId('undo-button')
+  fireEvent.click(undo)
+  fireEvent.click(undo)
+  expect(textarea.textContent).toContain('Then there was the bad weather.')
+})
+
+it('should be possible to redo undos', () => {
+  fireEvent.change(textarea, { target: { value: 'Hello world' } })
+  const undo = getByTestId('undo-button')
+  fireEvent.click(undo)
+  const redo = getByTestId('redo-button')
+  fireEvent.click(redo)
+  expect(textarea.textContent).toContain('Hello world')
+})
+
+it('should not crash when pressing redo twice', () => {
+  fireEvent.change(textarea, { target: { value: 'Hello world' } })
+  const undo = getByTestId('undo-button')
+  fireEvent.click(undo)
+  const redo = getByTestId('redo-button')
+  fireEvent.click(redo)
+  fireEvent.click(redo)
+  expect(textarea.textContent).toContain('Hello world')
+})
